0e6ea0274b69a78ba61fcc1660d6dcfab8706433
elson.mathew <elson.mathew@malauzai.com>
2019-08-26 13:41:37 -0500

    Initial Commit

---
+++ app/src/main/java/com/elsonmathew/android/nasaproject/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.elsonmathew.android.nasaproject
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
+	}
+}
